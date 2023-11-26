e3d516cbbd84ff12d2b746f2333874e3a2efd0d5
Karlaa28 <karlacamino2017.28@gmail.com>
2023-11-25 21:17:51 -0500

    1ercommit

---
+++ src/main/java/pe/edu/cibertec/DSWII_CL3_VargasKarla/DswiiCl3VargasKarlaApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.DSWII_CL3_VargasKarla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DswiiCl3VargasKarlaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DswiiCl3VargasKarlaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/DSWII_CL3_VargasKarla/controller/SeguridadController.java
@@ -0,0 +1,4 @@
+package pe.edu.cibertec.DSWII_CL3_VargasKarla.controller;
+
+public class SeguridadController {
+}
